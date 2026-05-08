07e9d2d2dc39d0ff4cb858de2d727855d2af28c0
xxxloveitxxx <socialorbit1@gmail.com>
2026-05-08 15:43:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/notes/', // 👈 Add this line
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 });
